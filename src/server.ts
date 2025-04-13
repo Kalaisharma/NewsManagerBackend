acbe7d1a334670fca0f8fa1c9c7bf3eff54ed3a4
Kalaisharma <99097741+Kalaisharma@users.noreply.github.com>
2025-04-13 17:22:53 +0530

    Update CORS middleware to allow all origins for debugging

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 //"https://mynewsmanager.netlify.app"
 // Middleware
-app.use(cors());
+app.use(cors({ origin: '*' })); // For wide open access during debugging
 app.use(express.json()); // Parse JSON requests
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded requests
 
